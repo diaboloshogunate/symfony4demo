75c209430e816e657f78bbab44a57f048d0034d0
Robert Parker <rparker@kodama.design>
2017-11-21 13:00:14 -0800

    captureing values

--- src/Controller/HelloWorld.php
+++ src/Controller/HelloWorld.php
@@ -16,4 +16,21 @@ class HelloWorld
         return new Response("Hello World");
     }
 
+    /**
+     * @Route("/hello/{name}", name="hello robot", requirements={"name": "\d+"})
+     * @return Response
+     */
+    public function robot($name)
+    {
+        return new Response(sprintf("Hello robot: %s.", $name));
+    }
+
+    /**
+     * @Route("/hello/{name}", name="hello human")
+     * @return Response
+     */
+    public function human($name = "john doe")
+    {
+        return new Response(sprintf("Hello human: %s.", $name));
+    }
 }
