ffc1f471374d1cac8bd3a66638a2710a9f8a91a2
Robert Parker <rparker@kodama.design>
2017-11-21 13:36:22 -0800

    profiler and twig

--- src/Controller/HelloWorld.php
+++ src/Controller/HelloWorld.php
@@ -11,26 +11,38 @@ class HelloWorld
      * @Route("/", name="hello world")
      * @return Response
      */
-    public function index()
+    public function index(\Twig_Environment $twig)
     {
-        return new Response("Hello World");
+        $data = [
+            'title' => 'Hello World',
+            'content' => '<h1>Hello World</h1>',
+        ];
+        return new Response($twig->render('page.html.twig', $data));
     }
 
     /**
      * @Route("/hello/{name}", name="hello robot", requirements={"name": "\d+"})
      * @return Response
      */
-    public function robot($name)
+    public function robot(\Twig_Environment $twig, $name)
     {
-        return new Response(sprintf("Hello robot: %s.", $name));
+        $data = [
+            'title' => 'Hello Robot',
+            'content' => '<h1>'.sprintf("Hello %s.", $name).'</h1>',
+        ];
+        return new Response($twig->render('page.html.twig', $data));
     }
 
     /**
      * @Route("/hello/{name}", name="hello human")
      * @return Response
      */
-    public function human($name = "john doe")
+    public function human(\Twig_Environment $twig, $name = "john doe")
     {
-        return new Response(sprintf("Hello human: %s.", $name));
+        $data = [
+            'title' => 'Hello Human',
+            'content' => '<h1>'.sprintf("Hello %s.", $name).'</h1>',
+        ];
+        return new Response($twig->render('page.html.twig', $data));
     }
 }
