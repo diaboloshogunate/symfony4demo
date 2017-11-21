b0c24234db99a838daca81c816b9924e1184500c
Robert Parker <rparker@kodama.design>
2017-11-21 12:42:10 -0800

    annotations

--- src/Controller/HelloWorld.php
+++ src/Controller/HelloWorld.php
@@ -2,9 +2,15 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloWorld
 {
+
+    /**
+     * @Route("/", name="hello world")
+     * @return Response
+     */
     public function index()
     {
         return new Response("Hello World");
